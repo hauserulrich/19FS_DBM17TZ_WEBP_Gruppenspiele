00e87c07faae5912e73150f446a0bcfbf18ae42e
RemoCode <thommen.remo@hotmail.com>
2019-05-08 19:11:46 +0200

    get groupnames
    js angepasst um gruppennamen zu erhalten

--- www/js/gruppenname.js
+++ www/js/gruppenname.js
@@ -1,16 +1,19 @@
-function init()
-{
+function init(){
     $.ajax({url: "http://767727-7.web1.fh-htwchur.ch/19FS_DBM17TZ_WEBP_Gruppenspiele/db/teamnames.php"}).done(receivedteamnames).fail(ajaxFailed);
 }
 
-function receivedteamnames(data)
-{
-    alert(data);
+function receivedteamnames(data){
+    var data_arr = JSON.parse(data);
+    var content = "";
+    for(var i in data_arr) {
+        inhalt = data_arr[i];
+        content += "<li>" + inhalt + "</li>";
+    }
+    $("#groupnames").html(content);
 }
 
-function ajaxFailed()
-{
-    
+function ajaxFailed(){
+    alert("fail");
 }
 
 window.onload=init;

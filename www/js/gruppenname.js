fb36ff6d5dd4366b0fd983ab4568ac157f051e6a
RemoCode <thommen.remo@hotmail.com>
2019-05-08 19:14:54 +0200

    Update gruppenname.js

--- www/js/gruppenname.js
+++ www/js/gruppenname.js
@@ -1,5 +1,5 @@
 function init(){
-    $.ajax({url: "http://767727-7.web1.fh-htwchur.ch/19FS_DBM17TZ_WEBP_Gruppenspiele/db/teamnames.php"}).done(receivedteamnames).fail(ajaxFailed);
+    $.ajax({url: "db/teamnames.php"}).done(receivedteamnames).fail(ajaxFailed);
 }
 
 function receivedteamnames(data){
